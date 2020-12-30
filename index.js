404b4272348aa69203459d1041584141750b8c28
Michaela Wagner <mickywagner@gmail.com>
2020-12-30 02:11:40 -0700

    Add open btn and js for nav

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const closeNavBtn = document.querySelector(".close-nav");
+const openNavBtn = document.querySelector(".open-nav")
 const nav = document.querySelector(".nav");
 
 closeNavBtn.addEventListener("click", () => {  
     nav.classList.remove("navigation-open")
 });
 
+openNavBtn.addEventListener("click", () => {  
+    nav.classList.add("navigation-open")
+});
